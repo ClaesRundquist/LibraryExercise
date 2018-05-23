c1a293c94451d2af2c48df62a389af6bd108f048
ClaesRundquist <claesrundquist@yahoo.se>
2018-05-23 13:22:03 +0200

    add thymeleaf css annotation add favicon

--- src/main/java/se/lexicon/library/controllers/createSearchWebController.java
+++ src/main/java/se/lexicon/library/controllers/createSearchWebController.java
@@ -1,17 +1,20 @@
 package se.lexicon.library.controllers;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import se.lexicon.library.domain.Member;
+import se.lexicon.library.domain.Book;
 
 @Controller
 public class createSearchWebController {
 
 	@GetMapping("/createSearchForm")
 	public String getForm(Model m) {
-		m.addAttribute("Member", new Member());
+		Book book=new Book();
+		book.setLoanPeriod(3L);
+		m.addAttribute("book", book);
 		return "bookCreate";
 	}
 
