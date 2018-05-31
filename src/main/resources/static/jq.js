70d19f0550f96c5c01d76fff551f45df392cab1f
ClaesRundquist <claesrundquist@yahoo.se>
2018-05-31 16:10:38 +0200

    add responsive menu

--- src/main/resources/static/jq.js
+++ src/main/resources/static/jq.js
@@ -1,3 +1,16 @@
+/* Javascript for menu nav from example https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_topnav  */
+
+$('#myTopnav').on("click", ".icon", function(){
+    console.log($(this).parent()+"hamburger");
+    var x= $(this).parent().get(0);
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+});
+    
+    
 /* JQuery functions for LibraryExercise project */
 function populate(frm, data) {
 	$.each(data, function(key, value) {
@@ -7,12 +20,14 @@ function populate(frm, data) {
 
 function createBookResultsTable(data) {
 	var content=[];
-	if (data=="null") {
+	if (data==null) {
 		; // do nothing, empty array will be passed to DataTable constructor.
 	} else if (data.books == undefined) {
 		// single row with book data
 		var cRow = [];
-		cRow.push('<button data-id="'+data.id+'">delete</button>');
+		cRow.push('<span><button class="deleteButton btn btn-default" data-id="'+data.id+'">Delete</button>'
+				+'<button class="updateButton btn btn-default" data-id="'+data.id+'">Update</button>'
+				+'<button class="cloneButton btn btn-default" data-id="'+data.id+'">Clone</button></span>');
 		cRow.push(data.id);
 		cRow.push(data.isbn);
 		cRow.push(data.title);
@@ -25,7 +40,9 @@ function createBookResultsTable(data) {
 		// Array of books.
 		$.each(data.books, function(key, value) {
 			var cRow = [];
-			cRow.push('<button data-id="'+value.id+'">delete</button>');
+			cRow.push('<span><button class="deleteButton btn btn-default" data-id="'+value.id+'">Delete</button>'
+					+'<button class="updateButton btn btn-default" data-id="'+value.id+'">Update</button>'
+					+'<button class="cloneButton btn btn-default" data-id="'+value.id+'">Clone</button></span>');
 			cRow.push(value.id);
 			cRow.push(value.isbn);
 			cRow.push(value.title);
@@ -40,7 +57,7 @@ function createBookResultsTable(data) {
 		destroy : true,
 		data : content,
 		columns : [ {
-			title : "Operation"
+			title : "Operations"
 		}, {
 			title : "Id"
 		}, {
@@ -52,9 +69,9 @@ function createBookResultsTable(data) {
 		}, {
 			title : "ISBN"
 		}, {
-			title : "Loacation"
+			title : "Location"
 		}, {
-			title : "LoanPeriod"
+			title : "Loan Period"
 		} ]
 	});
 
@@ -101,10 +118,66 @@ $("#searchITA").click(
 
 
 
-$('#bookResultsTable').on("click", "button", function(){
+$('#bookResultsTable').on("click", ".deleteButton", function(){
     console.log($(this).parent());
-    alert("deleting row w id="+$(this).attr('data-id'));
-    $(this).parents('tr').remove();
+
+    // global scoped flag to make use of this-relative traversing possible instead of DOM search.
+    bookResultsTableDelete=true;
+	$.ajax({
+		type : "delete",
+		dataType : "text",
+		url : "http://localhost:8080/book/delete/" + $(this).attr('data-id'),
+		success : function(data) {
+		},
+		failure : function(errMsg) {
+			bookResultsTableDelete=false;
+			$("#res").html(errMsg);
+		}
+	});
+
+	if (bookResultsTableDelete) {
+		$(this).parents('tr').remove();
+	}
+	
+});
+
+$('#bookResultsTable').on("click", ".updateButton", function(){
+    console.log($(this).parent()+"update");
+
+    location.href="http://localhost:8080/findToUpdate/" + $(this).attr('data-id');
+	
+});
+
+$('#bookResultsTable').on("click", ".cloneButton", function(){
+    console.log($(this).parent()+"clone");
+
+	$.ajax({
+		type : "post",
+		contentType : "application/json; charset=utf-8",
+		dataType : "text",
+		data : $(this).attr('data-id'),
+		url : "http://localhost:8080/book/clone",
+		success : function(data) {
+			var table = $('#bookResultsTable').DataTable();
+
+			var value=JSON.parse(data);
+			var cRow = [];
+			cRow.push('<span><button class="deleteButton btn btn-default" data-id="'+value.id+'">Delete</button>'
+					+'<button class="updateButton btn btn-default" data-id="'+value.id+'">Update</button>'
+					+'<button class="cloneButton btn btn-default" data-id="'+value.id+'">Clone</button></span>');
+			cRow.push(value.id);
+			cRow.push(value.isbn);
+			cRow.push(value.title);
+			cRow.push(value.author);
+			cRow.push(value.genre);
+			cRow.push(value.location);
+			cRow.push(value.loanPeriod);
+			
+			table.row.add(cRow).draw();
+
+		}
+	});
+
 });
 
 
